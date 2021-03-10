f71be794147c16b8dc5133b0672f2c46ac90e382
wang <43700853+qianwang0924@users.noreply.github.com>
2021-03-10 21:10:45 +0800

    Add files via upload

--- Bert-CNN.py
+++ Bert-CNN.py
@@ -59,7 +59,9 @@ def get_bert_input_V2(sentence,max_sentence_length):
     #print(bert_input)
     return bert_input
 
-##数据集处理模块##
+
+
+###########################################数据集处理模块################################################
 def get_label(label_path):
     pre_train_label = pd.read_csv(label_path,sep="\t",header=None,encoding="gbk")
     df = pd.DataFrame(pre_train_label)
@@ -70,7 +72,6 @@ def get_label(label_path):
     id_use = []
     id_unuse = []
     id_idx_unuse = []
-    
 
     #遍历整个行，一共有19600条数据
     for i in range(1,19600):
@@ -164,17 +165,127 @@ def convert_example_to_feature(text):
         attention_mask_list.append(bert_input['attention_mask'])
     return input_ids_list,token_type_ids_list,attention_mask_list
 
+model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased',num_labels = 3)
 
-def bert_cnn_model():
-    model = TFBertForSequenceClassification.from_pretrained(('bert-base-uncased', num_labels=3))
+
+def bert_model():
+
+    model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased',num_labels = 3)
+    model.get
+    
+    
+
+    
+
+    
+    
+    print(model.get_output_embeddings().shape)
+
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate,epsilon=1e-08, clipnorm=1)
+
+    
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
     model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+    bert_history = model.fit(ds_train_encoded, epochs=number_of_epochs, validation_data=ds_val_encoded)
 
 
-  
 
+
+##########################################################卷积神经网络部分##########################################
+
+
+def conv(input,name,kh,kw,n_out,dh, dw):#dh,dw为卷积核kernel的stride，kh和kw为卷积核kernel的长宽，n_out为卷积核的厚度，也是输出下一层的厚度
+    n_in = input.get_shape()[-1].value 
+    with tf.name_scope(name) as scope:
+        kernel = tf.Variable(scope+"w",
+                                 shape=[kh, kw, n_in, n_out],
+                                 dtype=tf.float32, 
+                                 initializer=tf.initializers.GlorotNorma(shape=shape))
+                                 
+        conv = tf.nn.conv2d(input, kernel, (1, dh, dw, 1), padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[n_out], dtype=tf.float32), trainable=True,name='b')
+        z = tf.nn.bias_add(conv, biases)
+        activation = tf.nn.relu(z, name=scope)
+        return activation
+
+def linear(input,name,n_out):#全连接操作,n_out为输出结点的个数
+    n_in = input.get_shape()[-1].value
+    with tf.name_scope(name) as scope:
+        kernel = tf.get_variable(scope+"w",
+                                 shape=[n_in, n_out],
+                                 dtype=tf.float32, 
+                                 initializer=tf.initializers.GlorotNorma(shape=shape))
+                                
+        biases = tf.Variable(tf.constant(0.1, shape=[n_out], dtype=tf.float32), name='b')
+        activation = tf.nn.relu(tf.matmul(input, kernel) + biases, name=scope)
+        return activation
+
+        
+def maxpooling(input, name, kh, kw, dh, dw):
+    return tf.nn.max_pool(input_op,
+                          ksize=[1, kh, kw, 1],
+                          strides=[1, dh, dw, 1],
+                          padding='SAME',
+                          name=name)
+
+
+def cnn_model(input,keep_prob):#keep_prob为dropout设置的参数，可以为一个全局变量
+    with tf.variable_scope('conv_1_1'):
+        conv1_1 = conv(input, name="conv1_1", kh=3, kw=3, n_out=64, dh=1, dw=1)
+    with tf.variable_scope('conv_1_2'):
+        conv1_2 = conv(conv1_1,  name="conv1_2", kh=3, kw=3, n_out=64, dh=1, dw=1)
+    with tf.variable_scope('pool_1'):
+        pool1 = maxpooling(conv1_2,   name="pool1",kh=2, kw=2, dw=2, dh=2)
+
+    
+    with tf.variable_scope('conv_2_1'):
+        conv2_1 = conv(pool1,name="conv2_1", kh=3, kw=3, n_out=128, dh=1, dw=1)
+    with tf.variable_scope('conv_2_2'):
+        conv2_2 = conv(conv2_1,  name="conv2_2", kh=3, kw=3, n_out=128, dh=1, dw=1)
+    with tf.variable_scope('pool_2'):
+        pool2 = maxpooling(conv2_2,   name="pool2",kh=2, kw=2, dh=2, dw=2)
+
+
+    with tf.variable_scope('conv_3_1'):
+        conv3_1 = conv(pool2, name="conv3_1", kh=3, kw=3, n_out=256, dh=1, dw=1)
+    with tf.variable_scope('conv_3_2'):
+        conv3_2 = conv(conv3_1, name="conv3_2", kh=3, kw=3, n_out=256, dh=1, dw=1)
+    with tf.variable_scope('conv_3_3'):
+        conv3_3 = conv(conv3_2, name="conv3_3", kh=3, kw=3, n_out=256, dh=1, dw=1)    
+    with tf.variable_scope('pool_3'):
+        pool3 = maxpooling(conv3_3, name="pool3", kh=2, kw=2, dh=2, dw=2)
+
+    with tf.variable_scope('conv_4_1'):
+        conv4_1 = conv(pool3, name="conv4_1", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    with tf.variable_scope('conv_4_2'):
+        conv4_2 = conv(conv4_1,  name="conv4_2", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    with tf.variable_scope('conv_4_3'):
+        conv4_3 = conv(conv4_2,  name="conv4_3", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    with tf.variable_scope('pool_4'):
+        pool4 = maxpooling(conv4_3,name="pool4", kh=2, kw=2, dh=2, dw=2)
+
+    with tf.variable_scope('conv_5_1'):
+        conv5_1 = conv(pool4,name="conv5_1", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    with tf.variable_scope('conv_5_2'):
+        conv5_2 = conv(conv5_1,  name="conv5_2", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    with tf.variable_scope('conv_5_3'):
+        conv5_3 = conv(conv5_2,  name="conv5_3", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    with tf.variable_scope('pool_5'):
+        pool5 = maxpooling(conv5_3, name="pool5", kh=2, kw=2, dw=2, dh=2)
+
+
+
+    with tf.variable_scope('gap'):#根据论文实现GAP替代全连接层实现分类
+        GAP = tf.nn.avg_pool2d(pool5 ,ksize=[1,pool5.get_shape().as_list()[1],pool5.get_shape().as_list()[1],1],strides=[1,1,1,1],padding='VALID',name='GAP')
+
+    with tf.variable_scope('fully_connect_1'):#全连接层1实现512到100
+        linear1 = linear(GAP, name="linear1", n_out=100)
+        linear1_drop = tf.nn.dropout(linear1, keep_prob, name="linear1_drop")
+
+    with tf.variable_scope('fully_connect_2'):#全连接层2实现100到3，实现情绪的分类
+        linear2 = linear(linear1_drop, name="linear2", n_out=3)
+        linear2_drop = tf.nn.dropout(linear2, keep_prob, name="linear2_drop")
 
 
 text_label,image_label,id_use =  get_label(label_path)
